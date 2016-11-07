7af7c10fc9b1812803c154eb1c358e45b3ef2df1
Bibek <jujulalshrestha@gmail.com>
2016-11-07 14:18:14 -0600

    Lab2 Complete

--- Lab2/src/mum/lab/names/AddNameServlet.java
+++ Lab2/src/mum/lab/names/AddNameServlet.java
@@ -19,43 +19,11 @@ public class AddNameServlet extends HttpServlet {
     public void init() {
      personList = new ArrayList<Person>();
     }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        out.print("<HTML><HEAD>");
-        out.print("<TITLE>Lab2 - AddNameServlet</TITLE>");
-        out.print("</HEAD><BODY>");
-        out.print("<div><h3>List of Names</h3>");
-        List<Person> personList = (List<Person>)request.getSession().getAttribute("personList");
-        if(personList != null) {
-            for (Person p : personList) {
-                out.print("Key: " + p.getKey() + " FirstName: " + p.getFirstName() + " LastName: " + p.getLastName() + "<br>");
-            }
-        } else{
-            personList = new ArrayList<>();
-        }
-        HttpSession session = request.getSession();
-        session.setAttribute("personList", personList);
-        out.print("</div>");
-        out.print("<div><FORM method='post' action='/addName'>");
-        out.print("Key: <INPUT type='text' name='key'> ");
-        out.print("First Name: <INPUT type='text' name='firstName'> ");
-        out.print("Last Name: <INPUT type='text' name='lastName'> ");
-        out.print("<INPUT type='submit' name='submit' value='Add'> ");
-        out.print("</FORM></div>");
-        out.print("</BODY></HTML>");
-    }
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-
-
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        //PrintWriter out = response.getWriter();
         HttpSession session = request.getSession(false);
-
         Person person = new Person(
                 new Integer(request.getParameter("key")),
                 request.getParameter("firstName"),
@@ -63,7 +31,7 @@ public class AddNameServlet extends HttpServlet {
         );
         personList.add(person);
         session.setAttribute("personList", personList);
-
-        doGet(request, response);
+        //response.sendRedirect("index.html");
+        //doGet(request, response);
     }
 }
--- Lab2/src/mum/lab/names/RemoveNameServlet.java
+++ Lab2/src/mum/lab/names/RemoveNameServlet.java
@@ -7,27 +7,34 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Bibek on 11/5/2016.
+ * Created by Bibek on 11/7/2016.
  */
-public class AllNamesServlet extends HttpServlet {
+public class RemoveNameServlet extends HttpServlet {
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html");
+
         PrintWriter out = response.getWriter();
 
-        List<Person> personList = (List<Person>)request.getSession().getAttribute("personList");
-        if(personList != null) {
-            out.print("<table border='1' padding='0' margin='0'><th>Key</th><th>FirstName</th><th>LastName</th>");
-            for (Person p : personList) {
-                out.print("<tr><td>" + p.getKey() + "</td><td>" + p.getFirstName() + "</td><td>" + p.getLastName() + "</td></tr>");
-            }
-        } else{
-            personList = new ArrayList<>();
-        }
         HttpSession session = request.getSession();
+        List<Person> personList = (List<Person>)request.getSession().getAttribute("personList");
+        boolean state = false;
+
+        for(Person p: personList){
+            String key = new String("" + p.getKey());
+            if(key.equals(request.getParameter("keyDelete"))){
+                personList.remove(p);
+                out.print("Record deleted Successfully");
+                state=true;
+            }
+        }
+
+        if(state =false){ out.print("Record not found!");}
+
         session.setAttribute("personList", personList);
     }
 }
